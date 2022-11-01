086813ccd88e422055b0e19d24a9bcdf0e0289df
Regino Fernández García <rfg@altair.edu.es>
2022-11-01 10:59:52 +0100

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using entityBasicoDAL;
+using EntityFrameworkIntroduccion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EntityFrameworkIntroduccion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AccesoDC context;
+
+        public HomeController(AccesoDC context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
